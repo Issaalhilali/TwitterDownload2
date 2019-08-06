7de537c5458284fdad38750c3b1ffa82543df236
issa12345 <issaalhiali@gmail.com>
2019-08-06 18:20:02 +0300

    update

---
+++ app/src/main/java/com/issa/twitterdownload/utils/ServiceUtil.java
@@ -0,0 +1,182 @@
+package com.issa.twitterdownload.utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import com.esafirm.rxdownloader.RxDownloader;
+import com.issa.twitterdownload.R;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.services.StatusesService;
+
+import retrofit2.Call;
+import rx.Observer;
+
+
+
+public class ServiceUtil {
+
+        private static Context c;
+
+
+        public static void fetchTweet(String copiedURL,Context context) {
+            c=context;
+            String fname;
+
+
+            //Check if the tweet url field has text containing twitter.com/...
+            if (copiedURL.length()>0 && copiedURL.contains("twitter.com/")) {
+
+                Long id = getTweetId(copiedURL);
+
+                //Call method to get tweet
+                if (id !=null) {
+                    //set the tweet Id as the filename
+                    fname = String.valueOf(id);
+
+                    getTweet(id, fname);
+                }
+
+            }else {
+                alertNoUrl();
+            }
+        }
+
+        private static Long getTweetId(String s) {
+            try {
+                String[] split = s.split("\\/");
+                String id = split[5].split("\\?")[0];
+                return Long.parseLong(id);
+            }catch (Exception e){
+                Log.d("TAG", "getTweetId: "+e.getLocalizedMessage());
+                alertNoUrl();
+                return null;
+            }
+        }
+
+        private static void alertNoVideo() {
+            Toast.makeText(c, c.getString(R.string.containt_tweet), Toast.LENGTH_LONG).show();
+        }
+
+        private static void alertNoMedia() {
+            Toast.makeText(c,c.getString(R.string.media_file), Toast.LENGTH_LONG).show();
+        }
+
+        private static void alertNoUrl() {
+            Toast.makeText(c,c.getString(R.string.not_tweet), Toast.LENGTH_LONG).show();
+        }
+
+
+        private static void showFetchingTweetNoti() {
+            Toast.makeText(c,c.getString(R.string.tch), Toast.LENGTH_LONG).show();
+        }
+
+        private static void getTweet(final Long id, final String fname){
+            showFetchingTweetNoti();
+
+            TwitterApiClient twitterApiClient= TwitterCore.getInstance().getApiClient();
+            StatusesService statusesService=twitterApiClient.getStatusesService();
+            Call<Tweet> tweetCall=statusesService.show(id,null,null,null);
+            tweetCall.enqueue(new Callback<Tweet>() {
+                @Override
+                public void success(Result<Tweet> result) {
+
+                    //Check if media is present
+                    if (result.data.extendedEtities==null && result.data.entities.media==null){
+                        alertNoMedia();
+                    }
+                    //Check if gif or mp4 present in the file
+                    else if (!(result.data.extendedEtities.media.get(0).type).equals("video") && !(result.data.extendedEtities.media.get(0).type).equals("animated_gif")){
+                        alertNoVideo();
+                    }
+                    else {
+                        String filename=fname;
+                        String url;
+
+                        //Set filename to gif or mp4
+                        if ((result.data.extendedEtities.media.get(0).type).equals("video")) {
+                            filename = filename + ".mp4";
+                        }else {
+                            filename = filename + ".gif";
+                        }
+
+                        int i=0;
+                        url = result.data.extendedEtities.media.get(0).videoInfo.variants.get(i).url;
+                        while (!url.contains(".mp4")){
+                            try {
+                                if (result.data.extendedEtities.media.get(0).videoInfo.variants.get(i) != null) {
+                                    url = result.data.extendedEtities.media.get(0).videoInfo.variants.get(i).url;
+                                    i += 1;
+                                }
+                            } catch (IndexOutOfBoundsException e) {
+                                downloadVideo(url,filename);
+                            }
+                        }
+
+                        downloadVideo(url,filename);
+                    }
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+                    Toast.makeText(c, c.getString(R.string.requset_failed), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+        private static void downloadVideo(String url, String fname) {
+
+
+            //Check if External Storage permission js allowed
+            if (!storageAllowed()) {
+                // We don't have permission so prompt the user
+//            ActivityCompat.requestPermissions(c, Constant.PERMISSIONS_STORAGE, Constant.REQUEST_EXTERNAL_STORAGE);
+                Toast.makeText(c,c.getString(R.string.kindly), Toast.LENGTH_SHORT).show();
+            }else {
+                RxDownloader.getInstance(c)
+                        .download(url,fname, "video/*") // url, filename, and mimeType
+                        .subscribe(new Observer<String>() {
+                            @Override
+                            public void onCompleted() {
+                                Toast.makeText(c,c.getString(R.string.completed) , Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Toast.makeText(c, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onNext(String s) {
+
+                            }
+                        });
+
+                Toast.makeText(c,c.getString(R.string.check_notification) , Toast.LENGTH_LONG).show();
+            }
+        }
+
+        private static boolean storageAllowed() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                int permission = ActivityCompat.checkSelfPermission(c, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                return permission == PackageManager.PERMISSION_GRANTED;
+
+            }
+
+            return true;
+        }
+
+    }
+
